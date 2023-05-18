b657b96355354da485f0f3dfffd0e5182bf4bf40
pammitu <pamstankova@gmail.com>
2023-05-18 10:11:32 -0700

    fixing errors on favorites

--- src/components/FavoritesPage.js
+++ src/components/FavoritesPage.js
@@ -2,24 +2,21 @@ import React from "react";
 import IngredientsPage from "./IngredientsPage";
 
 function FavoritesPage() {
-
-    return (
-        <div>
-            <h1>Favorite Page</h1>
-            <p>List your favorite items here!</p>
-        </div>
-    );
-}
-
-
-const [favorites, setFavorites] = useState([]);
+const [favorite, setFavorites] = useState([]);
 
 const addToFavorites = ingredient => {
     setFavorites([...favorites, ingredient]);
 }
 
-favorites.map(favorite => (
-    <p>{favorite.name}</p>
-))
+    return (
+        <div>
+            <h1>Favorite Page</h1>
+            <p>List your favorite items here!</p>
+            {favorite.map(favorite => (
+                <p>{favorite.name}</p>
+            ))}
+        </div>
+    );
+}
 
 export default FavoritesPage;
