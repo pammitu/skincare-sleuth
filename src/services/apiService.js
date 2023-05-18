3d7030883bd1a8bad9204c6f5f4f69b9348a86b0
pammitu <pamstankova@gmail.com>
2023-05-17 19:50:10 -0700

    implementing API calls

---
+++ src/services/apiService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const BASE_URL = 'https://world.openfoodfacts.org/data';
+//fetch data for certain product
+export const getProductData = async (productID) => {
+    try {
+        const response = await axios.get(`${baseURL}${productId}.json`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+}
+//function to fetch info for set of products
+
+export const getProductsData = async (searchQuery) => {
+    try {
+        const response = await axios.get(`${baseURL}?search_terms=${searchQuery}&search_simple=1&action=process&json=1`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error: ${error}`); 
+    }
+}
